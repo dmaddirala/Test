608ee2f1c426362a53040f373f73cadf33b6ba50
Dhruv Maddirala <dhruv.maddirala@aepl.com>
2021-07-31 03:36:13 +0530

    Functions Implemented, Names Changed

--- QuestionRepository.cs
+++ QuestionRepository.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace QnA
 {
-    class QuestionRepository
+    class Repository
     {
         private List<Question> Questions { get; set; }
 
-        public QuestionRepository(){
+        public Repository(){
             this.Questions = new List<Question>();
         }
 
@@ -17,6 +17,17 @@ namespace QnA
             Questions.Add(new Question("What is CarTrade?", "Bhavartha","bhavartha@carwale.com", DateTime.Now));
             Questions.Add(new Question("What is BikeWale?", "Dhruv","dhruv.maddirala@carwale.com", DateTime.Now));
             return Questions;
+        }
+
+        public void AddQuestion(Question question)
+        {
+            Questions.Add(question);
+        }
+
+        public void UpdateQuestion(Question question, string title, DateTime updateDate)
+        {
+            question.Title = title;
+            question.UpdateDate = updateDate;
         }
     }
 }
