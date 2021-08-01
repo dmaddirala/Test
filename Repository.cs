2262beca4090a0f100412e63589a090ea9cc78a0
Dhruv Maddirala <dhruv.maddirala@carwale.com>
2021-08-01 20:45:06 +0530

    Utils functions

--- Repository.cs
+++ Repository.cs
@@ -13,9 +13,6 @@ namespace QnA
 
         public List<Question> GetQuestions()
         {
-            Questions.Add(new Question("What is CarWale?", "Dhruv","dhruv.maddirala@carwale.com", DateTime.Now));
-            Questions.Add(new Question("What is CarTrade?", "Bhavartha","bhavartha@carwale.com", DateTime.Now));
-            Questions.Add(new Question("What is BikeWale?", "Dhruv","dhruv.maddirala@carwale.com", DateTime.Now));
             return Questions;
         }
 
