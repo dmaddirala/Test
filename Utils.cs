4d4aa6cc50d2ddd16f5994710f19a8a44a253567
Dhruv Maddirala <dhruv.maddirala@carwale.com>
2021-08-01 20:48:39 +0530

    Utils functions

--- Utils.cs
+++ Utils.cs
@@ -5,6 +5,13 @@ namespace QnA
 {
     class Utils
     {
+        public List<Question> GenerateQuestions(){
+            var Questions = new List<Question>() ;
+            Questions.Add(new Question("What is CarWale?", "Dhruv","dhruv.maddirala@carwale.com", DateTime.Now));
+            Questions.Add(new Question("What is CarTrade?", "Bhavartha","bhavartha@carwale.com", DateTime.Now));
+            Questions.Add(new Question("What is BikeWale?", "Dhruv","dhruv.maddirala@carwale.com", DateTime.Now));
+            return Questions;
+        }
         
     }
 
